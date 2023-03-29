3e76f915159d63e1f64b81b76a9ff2b84f8cdce7
ardzbk <118845295+ardzbk@users.noreply.github.com>
2023-03-29 17:32:19 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,212 @@
+
+const contantimg1 = document.querySelectorAll('.animation2')
+window.addEventListener('scroll', checkBoxes2)
+checkBoxes2()
+
+function checkBoxes2() {
+    const triggerBottom = window.innerHeight / 5 * 4
+    contantimg1.forEach(animation2 => {
+        const animationTop = animation2.getBoundingClientRect().top
+        if (animationTop < triggerBottom) {
+            animation2.classList.add('show')
+        } else {
+            animation2.classList.remove('show')
+        }
+    })
+}
+
+const contantimg2 = document.querySelectorAll('.animation3')
+window.addEventListener('scroll', checkBoxes3)
+checkBoxes3()
+
+function checkBoxes3() {
+    const triggerBottom = window.innerHeight / 5 * 4
+    contantimg2.forEach(animation3 => {
+        const animationTop = animation3.getBoundingClientRect().top
+        if (animationTop < triggerBottom) {
+            animation3.classList.add('show')
+        } else {
+            animation3.classList.remove('show')
+        }
+    })
+}
+
+const contantimg3 = document.querySelectorAll('.animation4')
+window.addEventListener('scroll', checkBoxes4)
+checkBoxes4()
+
+function checkBoxes4() {
+    const triggerBottom = window.innerHeight / 5 * 4
+    contantimg3.forEach(animation4 => {
+        const animationTop = animation4.getBoundingClientRect().top
+        if (animationTop < triggerBottom) {
+            animation4.classList.add('show')
+        } else {
+            animation4.classList.remove('show')
+        }
+    })
+}
+
+const contantdiv1 = document.querySelectorAll('.animation5')
+window.addEventListener('scroll', checkBoxes5)
+checkBoxes5()
+
+function checkBoxes5() {
+    const triggerBottom = window.innerHeight / 5 * 4
+    contantdiv1.forEach(animation5 => {
+        const animationTop = animation5.getBoundingClientRect().top
+        if (animationTop < triggerBottom) {
+            animation5.classList.add('show')
+        } else {
+            animation5.classList.remove('show')
+        }
+    })
+}
+
+const contantdiv2 = document.querySelectorAll('.animation6')
+window.addEventListener('scroll', checkBoxes6)
+checkBoxes6()
+
+function checkBoxes6() {
+    const triggerBottom = window.innerHeight / 5 * 4
+    contantdiv2.forEach(animation6 => {
+        const animationTop = animation6.getBoundingClientRect().top
+        if (animationTop < triggerBottom) {
+            animation6.classList.add('show')
+        } else {
+            animation6.classList.remove('show')
+        }
+    })
+}
+
+const contantdiv3 = document.querySelectorAll('.animation7')
+window.addEventListener('scroll', checkBoxes7)
+checkBoxes7()
+
+function checkBoxes7() {
+    const triggerBottom = window.innerHeight / 5 * 4
+    contantdiv3.forEach(animation7 => {
+        const animationTop = animation7.getBoundingClientRect().top
+        if (animationTop < triggerBottom) {
+            animation7.classList.add('show')
+        } else {
+            animation7.classList.remove('show')
+        }
+    })
+}
+
+const contantdiv4 = document.querySelectorAll('.animation8')
+window.addEventListener('scroll', checkBoxes8)
+checkBoxes8()
+
+function checkBoxes8() {
+    const triggerBottom = window.innerHeight / 5 * 4
+    contantdiv4.forEach(animation8 => {
+        const animationTop = animation8.getBoundingClientRect().top
+        if (animationTop < triggerBottom) {
+            animation8.classList.add('show')
+        } else {
+            animation8.classList.remove('show')
+        }
+    })
+}
+
+const contant5 = document.querySelectorAll('.animation9')
+window.addEventListener('scroll', checkBoxes9)
+checkBoxes9()
+
+function checkBoxes9() {
+    const triggerBottom = window.innerHeight / 5 * 4
+    contant5.forEach(animation9 => {
+        const animationTop = animation9.getBoundingClientRect().top
+        if (animationTop < triggerBottom) {
+            animation9.classList.add('show')
+        } else {
+            animation9.classList.remove('show')
+        }
+    })
+}
+
+const contant6 = document.querySelectorAll('.animation10')
+window.addEventListener('scroll', checkBoxes10)
+checkBoxes10()
+
+function checkBoxes10() {
+    const triggerBottom = window.innerHeight / 5 * 4
+    contant6.forEach(animation10 => {
+        const animationTop = animation10.getBoundingClientRect().top
+        if (animationTop < triggerBottom) {
+            animation10.classList.add('show')
+        } else {
+            animation10.classList.remove('show')
+        }
+    })
+}
+
+const contant7 = document.querySelectorAll('.animation11')
+window.addEventListener('scroll', checkBoxes11)
+checkBoxes11()
+
+function checkBoxes11() {
+    const triggerBottom = window.innerHeight / 5 * 4
+    contant7.forEach(animation11 => {
+        const animationTop = animation11.getBoundingClientRect().top
+        if (animationTop < triggerBottom) {
+            animation11.classList.add('show')
+        } else {
+            animation11.classList.remove('show')
+        }
+    })
+}
+
+
+const contant8 = document.querySelectorAll('.animation12')
+window.addEventListener('scroll', checkBoxes12)
+checkBoxes12()
+
+function checkBoxes12() {
+    const triggerBottom = window.innerHeight / 5 * 4
+    contant8.forEach(animation12 => {
+        const animationTop = animation12.getBoundingClientRect().top
+        if (animationTop < triggerBottom) {
+            animation12.classList.add('show')
+        } else {
+            animation12.classList.remove('show')
+        }
+    })
+}
+
+const contant9 = document.querySelectorAll('.animation13')
+window.addEventListener('scroll', checkBoxes13)
+checkBoxes13()
+
+function checkBoxes13() {
+    const triggerBottom = window.innerHeight / 5 * 4
+    contant9.forEach(animation13 => {
+        const animationTop = animation13.getBoundingClientRect().top
+        if (animationTop < triggerBottom) {
+            animation13.classList.add('show')
+        } else {
+            animation13.classList.remove('show')
+        }
+    })
+}
+
+
+const contant10 = document.querySelectorAll('.animation14')
+window.addEventListener('scroll', checkBoxes14)
+checkBoxes14()
+
+function checkBoxes14() {
+    const triggerBottom = window.innerHeight / 5 * 4
+    contant10.forEach(animation14 => {
+        const animationTop = animation14.getBoundingClientRect().top
+        if (animationTop < triggerBottom) {
+            animation14.classList.add('show')
+        } else {
+            animation14.classList.remove('show')
+        }
+    })
+}
+
+
